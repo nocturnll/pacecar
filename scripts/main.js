b47129f0e6bb5663e98b891b2b7b1e488666ce2f
Soleil Carter <msbelanger93@gmail.com>
2020-05-10 15:12:57 -0700

    Initial Commit Definitely By Soleil

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+import { firstPage } from './story.jsx';
+import { renderToStaticMarkup } from 'react-dom/server';
+
+const start = document.getElementById('start');
+const currentDialog = document.querySelectorAll('.dialog')[0]; // the dialog of the page you're on
+const currentChoices = document.querySelectorAll('.choices')[0]; // the choices for the page you're on
+const storyLog = document.getElementById('storyLog'); // a history of past dialog and choices made
+
+let currentPage = firstPage;
+
+start.addEventListener('click', () => startGame()) // pressing the start button
+
+const startGame = () => {
+	storyLog.innerHTML = '';
+	currentPage = firstPage; // Reset to page 0
+	start.style.display = 'none'; // hides the start button
+	loadCurrentPage(); // loads first page
+}
+
+const loadCurrentPage = () => {
+	currentChoices.innerHTML = '';
+	currentDialog.innerHTML = renderToStaticMarkup(currentPage.dialog); // loads the current page dialog
+	currentPage.choices.forEach(choice => { // iterates through choices for that page
+		const choiceElem = document.createElement('span'); // creates a span
+		choiceElem.classList.add('choice'); // makes span class='choice'
+		choiceElem.innerHTML = choice.name; // loads the current page choices
+		choiceElem.addEventListener('click', () => { // when you select a choice
+			historyPush(currentPage, choice);
+			currentPage = choice.targetPage; // set current page index to the target page
+			loadCurrentPage(); // load that page
+		})
+		currentChoices.append(choiceElem); // makes the choices appear
+	});
+
+	const playAgainButton = document.getElementById('play-again');
+	if (playAgainButton) {
+		playAgainButton.addEventListener('click', startGame);
+	}
+}
+
+const historyPush = (page, choice) => {
+	const latestPage = document.createElement('p');
+	latestPage.classList.add('latestPage');
+	latestPage.innerHTML = renderToStaticMarkup(page.dialog);
+
+	const latestChoice = document.createElement('p');
+	latestChoice.classList.add('latestChoice');
+	latestChoice.innerHTML = choice.action;
+
+	const historyItem = document.createElement('div');
+	historyItem.classList.add('history-item')
+	historyItem.append(latestPage, latestChoice);
+	storyLog.prepend(historyItem);
+}
---
+++ scripts/story.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import { Page } from './Page';
+
+const causeOfDeath = (cause) => Page.dialog(<>{cause}<p className="death-message">You died :rip:</p><p id="play-again">Play Again</p></>);
+
+const atTheStart = Page
+	.dialog(<p>You are at the <b>starting</b> line. It's time to go.</p>)
+	.addChoice('Gas', 'You press down on the gas pedal.', () => startingToMove)
+	.addChoice('Do nothing', 'You do nothing', () => TBD)
+
+const startingToMove = Page.dialog(<><p>Your car lurches forward. You quickly pick up the pace. The racecars behind you follow slowly.</p><p>It's a straightaway.</p></>)
+	.addChoice('Even more gas', 'You really put the pedal to the medal.', () => TBD)
+	.addChoice('Brake', 'You slam on the brakes.', () => brakingInTheStraightaway)
+	.addChoice('Turn right', 'You turn the wheel as hard as you can to the right.', () => turnRightIntoWall)
+
+const brakingInTheStraightaway = Page.dialog(<><p>You hit the brake. Your car stops in the middle of the straightaway. The racecars behind you slam on their brakes. You can sense their confusion.</p><p>You should keep going. This is no place to stop.</p></>)
+	.addChoice('Gas', 'You put your foot back on the gas.', () => TBD)
+	.addChoice('Do nothing', 'You do nothing.', () => TBD)
+
+const turnRightIntoWall = causeOfDeath(<><p>You crank the wheel as far as you can to the right at 60 miles per hour.</p><p>"Imagine a world like that," Ariana croons as your car careens into a wall.</p><p>"Imagine a world like that," she repeats, your car flipping end-over-end into the crowd.</p><p>Imagine.</p></>)
+
+const TBD = Page.dialog(<p>There's nothing here.<br />Moooore Features Coming Soon To a Pace Car Near You!</p>)
+
+export const firstPage = atTheStart;
