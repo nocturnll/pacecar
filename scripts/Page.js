5358bf15438a3f65950ea56cca439b28c2581acc
Soleil Carter <msbelanger93@gmail.com>
2020-05-10 15:36:44 -0700

    pretty code?

--- scripts/Page.js
+++ scripts/Page.js
@@ -1,21 +1,21 @@
-const makeChoice = (name, action, getTargetPage) => ({ 
-	name,
-	action,
-	get targetPage() {
-		return getTargetPage();
-	}
+const makeChoice = (name, action, getTargetPage) => ({
+  name,
+  action,
+  get targetPage() {
+    return getTargetPage();
+  },
 });
 
 export class Page {
-	constructor(dialog) {
-		this.dialog = dialog;
-		this.choices = [];
-	}
+  constructor(dialog) {
+    this.dialog = dialog;
+    this.choices = [];
+  }
 
-	addChoice(name, action, getTargetPage) {
-		this.choices.push(makeChoice(name, action, getTargetPage));
-		return this;
-	}
+  addChoice(name, action, getTargetPage) {
+    this.choices.push(makeChoice(name, action, getTargetPage));
+    return this;
+  }
 }
 
 Page.dialog = (d) => new Page(d);
