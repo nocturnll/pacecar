6157afdd6464fac5943f2f4d9258d5e8bd4296d3
Soleil Carter <msbelanger93@gmail.com>
2020-05-10 21:24:22 -0700

    more dialog

--- scripts/story.jsx
+++ scripts/story.jsx
@@ -6,28 +6,35 @@ const causeOfDeath = (cause) =>
   Page.dialog(
     <>
       {cause}
-      <p className="death-message">You died :rip:</p>
-      <p id="play-again">Play Again</p>
+      <p className="death-message">Game over</p>
+      <p id="play-again">Play again?</p>
     </>,
   );
 
 const atTheStart = Page.dialog(
-  <p>
-    You are at the <b>begin</b> line. It's time to go.
-  </p>,
+  <>
+    <p>
+      It's a beautiful day at the West Cordovan Grand Prix. As the pace car driver, it is up to you to lead the pack safely around the entire course. Once you have all completed the first lap, the
+      true race may begin.
+    </p>
+    <p>
+      You are at the <b>starting</b> line. It's time to go.
+    </p>
+    ,
+  </>,
 )
   .addChoice('Gas', 'You press down on the gas pedal.', () => startingToMove)
-  .addChoice('Do nothing', 'You do nothing', () => TBD);
+  .addChoice('Do nothing', 'You do nothing.', () => TBD);
 
 const startingToMove = Page.dialog(
   <>
-    <p>Your car lurches forward. You quickly pick up the pace. The racecars behind you follow slowly.</p>
-    <p>It's a straightaway.</p>
+    <p>Your car lurches forward and quickly picks up the pace. The racecars behind you roll to a cautious start.</p>
+    <p>The course begins with a straightaway.</p>
   </>,
 )
   .addChoice('Even more gas', 'You really put the pedal to the medal.', () => TBD)
   .addChoice('Brake', 'You slam on the brakes.', () => brakingInTheStraightaway)
-  .addChoice('Turn right', 'You turn the wheel as hard as you can to the right.', () => turnRightIntoWall);
+  .addChoice('Turn right', 'You turn the wheel as hard as you can to the right.', () => TBD);
 
 const brakingInTheStraightaway = Page.dialog(
   <>
@@ -36,7 +43,67 @@ const brakingInTheStraightaway = Page.dialog(
   </>,
 )
   .addChoice('Gas', 'You put your foot back on the gas.', () => TBD)
-  .addChoice('Do nothing', 'You do nothing.', () => TBD);
+  .addChoice('Do nothing', 'You do nothing.', () => doingNothingOnTheStraightaway);
+
+const doingNothingOnTheStraightaway = Page.dialog(
+  <>
+    <p>You sit in the car. It's a nice day. There's barely a cloud in the sky.</p>
+    <p>You can hear confusion in the announcer's voice. The crowd is agitated. Somewhere behind you, a car honks.</p>
+  </>,
+)
+  .addChoice('Gas', 'You put your foot back on the gas.', () => TBD)
+  .addChoice('Turn on the radio', 'You turn the radio on.', () => turnOnTheRadioOnTheStraightaway);
+
+const turnOnTheRadioOnTheStraightaway = Page.dialog(
+  <>
+    <p>You turn on the radio. Are pacecars supposed to have radios? Yours does. It's tuned to the local Top 40 Pop station. </p>
+    <p>The outside world falls away. It's just you, the car, the empty road ahead, and Ariana Grande.</p>
+  </>,
+)
+  .addChoice('Gas', 'You put your foot back on the gas.', () => radioOnDrivingStraightaway)
+  .addChoice('Change the station', 'You change the radio station.', () => TBD)
+  .addChoice('Do nothing', 'You do nothing.', () => radioOnDoingNothingStraightaway);
+
+const radioOnDoingNothingStraightaway = Page.dialog(
+  <>
+    <p>"Imagine a world like that," croons Ariana. Indeed.</p>
+    <p>There's a tap on your window. A man stands outside your car, gesticulating wildly.</p>
+  </>,
+)
+  .addChoice('Roll down the window', 'You roll down the window.', () => TBD)
+  .addChoice('Gas', 'You put your foot back on the gas.', () => TBD)
+  .addChoice('Do nothing', 'You continue to do nothing.', () => radioOnManAtWindowStraightaway);
+
+const radioOnManAtWindowStraightaway = Page.dialog(
+  <>
+    <p>You look at the man. The man looks at you.</p>
+    <p>He pulls on your car door. It's locked. He bangs on the window. Nothing happens. You are safe in your car.</p>
+  </>,
+)
+  .addChoice('Change the station', 'You change the radio station.', () => TBD)
+  .addChoice('Roll down the window', 'You roll down the window.', () => TBD)
+  .addChoice('Do nothing', 'You continue to do nothing at all.', () => radioOnManAtWindowDoNothingStraightaway);
+
+const radioOnManAtWindowDoNothingStraightaway = causeOfDeath(
+  // death
+  <>
+    <p>
+      You tap the steering wheel in time with the music. More men appear outside your windows. Ariana fades out and in her wake you can hear the crowd, the announcer, the other drivers -- they don't
+      know what to make of you.
+    </p>
+    <p>I don't know what to make of you.</p>
+  </>,
+);
+
+const radioOnDrivingStraightaway = Page.dialog(
+  <>
+    <p>Ariana hits a high note. You hit the gas.</p>
+    <p>The cars behind you start up again. In the distance, a booming, crackling voice reassures the crowd.</p>
+    <p>You're still on a straightaway.</p>
+  </>,
+)
+  .addChoice('Gas', 'You press harder on the gas pedal.', () => TBD)
+  .addChoice('Turn right', 'You crank the steering wheel all the way to the right.', () => turnRightIntoWall);
 
 const turnRightIntoWall = causeOfDeath(
   <>
